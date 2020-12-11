8a3e9c6c2ce0ba1f6637bf702dff9f57e8934190
Chintan Ganeshwala <chinta.ganeshwala@gmail.com>
2020-12-10 22:13:17 -0500

    Condition on Reservation to Borrowed

--- ProjectDeliverable3/src/com/model/checkBorrowStatus.java
+++ ProjectDeliverable3/src/com/model/checkBorrowStatus.java
@@ -36,14 +36,16 @@ public class checkBorrowStatus extends HttpServlet {
 		Date d = new Date();
 		String time=dataFormat.format(d);
 		System.out.println("====>"+Integer.parseInt(time));
-		if(Integer.parseInt(time)>=18) {
-			try {
-				System.out.println("=======>rid="+rid+"dcid"+dcid+"copy"+copyNumber+"branch"+branchId+"<========");
-				connect.cancelReservation(rid, dcid, copyNumber, branchId);
-				response.sendRedirect("ReserveBook.jsp?reserve=fail");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		if(url!=null) {
+			if(Integer.parseInt(time)>=18) {
+				try {
+					System.out.println("=======>rid="+rid+"dcid"+dcid+"copy"+copyNumber+"branch"+branchId+"<========");
+					connect.cancelReservation(rid, dcid, copyNumber, branchId);
+					response.sendRedirect("ReserveBook.jsp?reserve=fail");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 		else
